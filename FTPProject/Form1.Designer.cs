9f221d27c230eceb2815ecea69c817edada2e68a
Absolutely <851248673@qq.com>
2019-09-26 16:08:36 +0800

    添加ftp操作

---
+++ FTPProject/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace FTPProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lab_result = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txt_value = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(303, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "下载";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lab_result
+            // 
+            this.lab_result.AutoSize = true;
+            this.lab_result.Location = new System.Drawing.Point(32, 167);
+            this.lab_result.Name = "lab_result";
+            this.lab_result.Size = new System.Drawing.Size(53, 12);
+            this.lab_result.TabIndex = 1;
+            this.lab_result.Text = "返回值：";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(303, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "上传";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txt_value
+            // 
+            this.txt_value.Location = new System.Drawing.Point(34, 24);
+            this.txt_value.Multiline = true;
+            this.txt_value.Name = "txt_value";
+            this.txt_value.Size = new System.Drawing.Size(253, 69);
+            this.txt_value.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(34, 111);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "目录存在";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(129, 112);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 22);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "创建目录";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 283);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txt_value);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lab_result);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lab_result;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txt_value;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ FTPProject/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FTPProject
+{
+    public partial class Form1 : Form
+    {
+        FTPHelper Helper = new FTPHelper();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var text = txt_value.Text;
+            if (string.IsNullOrEmpty(text))
+            {
+                txt_value.Focus();
+                MessageBox.Show("请输入文件名称");
+                return;
+            }
+            var result = Helper.DownLoad(text);
+            lab_result.Text += result;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var text = txt_value.Text;
+            if (string.IsNullOrEmpty(text))
+            {
+                txt_value.Focus();
+                MessageBox.Show("请输入内容");
+                return;
+            }
+            var fileName = text.Substring(0, 1) + ".txt";
+            var f=Helper.UpLoad(fileName,text);
+            lab_result.Text += f ? "上传成功" : "上传失败";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var text = txt_value.Text;
+            if (string.IsNullOrEmpty(text))
+            {
+                txt_value.Focus();
+                MessageBox.Show("请输入文件夹名称");
+                return;
+            }
+            var result = Helper.Exists(text);
+            lab_result.Text += result?"存在":"不存在";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var text = txt_value.Text;
+            if (string.IsNullOrEmpty(text))
+            {
+                txt_value.Focus();
+                MessageBox.Show("请输入文件夹名称");
+                return;
+            }
+            var result = Helper.CreateDir(text);
+            lab_result.Text += result ? "成功" : "失败";
+        }
+    }
+}
